5af06cc3297dbc865d0abe107a8679dc38a65c73
Nejc <loncarnej@gmail.com>
2023-10-24 20:25:57 +0200

    Novice in popravek cenika

--- src/app/novice/novice.component.ts
+++ src/app/novice/novice.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, AfterViewInit, Input, ViewChildren, QueryList,ViewEncapsulation } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Renderer2, ViewChildren, QueryList,ViewEncapsulation } from '@angular/core';
 import { DatabaseService } from '../database.service';
 import { ActivatedRoute, Router } from '@angular/router';
+
 
 @Component({
   selector: 'app-novice',
@@ -10,14 +11,18 @@ import { ActivatedRoute, Router } from '@angular/router';
 export class NoviceComponent implements OnInit, AfterViewInit {
 
   Novica: Array<{id: string, position: Number, naslovna: string, naslov: string, vsebina: string}>;
+  id: string | number
   sub: any;
 
   constructor(private dbService: DatabaseService,
               private route: ActivatedRoute,
-              private router: Router) { }
+              private renderer: Renderer2) { }
 
   ngOnInit() {
-
+    this.route.queryParams.subscribe(params => {
+      this.id = params['id'];
+      console.log('ID:', this.id);
+    });
     this.dbService.readNovice().subscribe(
       (data) => {
         this.Novica = data;
@@ -34,6 +39,7 @@ export class NoviceComponent implements OnInit, AfterViewInit {
       });
        }
     );
+    this.scrollToTop();
   }
 
   ngAfterViewInit(){
@@ -43,7 +49,12 @@ export class NoviceComponent implements OnInit, AfterViewInit {
       top = null;
     }
   }
-
+  private scrollToTop() {
+    if (window) {
+      // Use Renderer2 to avoid direct manipulation of the DOM
+      this.renderer.setProperty(window, 'scrollTo', { top: 0, behavior: 'smooth' });
+    }
+  }
   scroll() {
 
   }
